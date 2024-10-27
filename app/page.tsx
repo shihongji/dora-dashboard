16a4f83713ef45a50c66312a92442f24309b95c2
hongji <shihongji21@gmail.com>
2024-10-27 12:55:00 -0700

    add banner, but hardcoded

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,29 @@ import { GameInfo, GameInsight } from "@data/GenData";
 import gameInfoArrayFile from "@data/gameInfo.json";
 import gameInsightArrayFile from "@data/gameInsights.json";
 import Header from "@components/Header";
+import BannerContainer from "@components/Banner";
 
+/**
+ * The `Home` component serves as the main page for the dashboard application.
+ * It manages the state and updates for game information and insights, and 
+ * renders the header, banner, pad, player, and feed components.
+ *
+ * State:
+ * - `currentSeconds`: Tracks the current time in seconds.
+ * - `pinnedInsight`: Stores the currently pinned game insight.
+ * - `gameInfo`: Stores partial game information.
+ * - `gameInsights`: Stores an array of game insights.
+ *
+ * Functions:
+ * - `handleItemClick`: Handles the event when an item is clicked in the feed, 
+ *   setting the pinned insight based on the clicked item's ID.
+ * - `updateGameInfo`: Updates the game information based on the current time.
+ * - `updateGameInsights`: Updates the game insights based on the current time.
+ * - `handleTimeUpdate`: Handles the time update by adjusting the current seconds 
+ *   and updating game information and insights.
+ *
+ * @returns {JSX.Element} The rendered Home component.
+ */
 export default function Home() {
   const [, setCurrentSeconds] = useState(0);
   const [pinnedInsight, setPinnedInsight] = useState<GameInsight | null>(null);
@@ -37,6 +59,7 @@ export default function Home() {
     return;
   };
 
+
   gameInsightArrayFile.sort((a, b) => a.video_time! - b.video_time!);
   const updateGameInsights = (seconds: number): void => {
     // loop the array, append to the gameInsights
@@ -59,6 +82,12 @@ export default function Home() {
     return;
   };
 
+  
+  /**
+   * Handles the time update by adjusting the current seconds and updating game information and insights.
+   *
+   * @param {number} seconds - The current time in seconds.
+   */
   const handleTimeUpdate = (seconds: number) => {
     setCurrentSeconds(seconds + 3 * 12 * 60);
     updateGameInfo(seconds + 3 * 12 * 60);
@@ -68,6 +97,7 @@ export default function Home() {
   return (
     <div>
       <Header />
+      <BannerContainer pinText="引援 ，伤病预警， 细节的球员特点「训练大模块」demo 阶段未展现，依赖后续大规模训练算法调优。" />
     <div className="p-4 h-screen">
       {/* Row 1: Pad and Player horizontally */}
       <div className="flex space-x-4">
