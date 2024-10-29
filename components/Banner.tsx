cbcfad61bd6937447a556cfd1dd4bb56e0d2d9c4
hongji <shihongji21@gmail.com>
2024-10-29 00:12:04 -0700

    wrap the localStorage in useEffect()

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,17 +1,22 @@
 "use client";
 
 import { Banner } from "flowbite-react";
+import { useEffect, useState } from "react";
 import { HiX } from "react-icons/hi";
 
 const BannerContainer = () => {
-  const pinText = localStorage.getItem("bannerText");
+  const [bannerText, setBannerText] = useState("");
+  useEffect(() => {
+    const pinText = localStorage.getItem("bannerText") || "Welcome to the dashboard!";
+    setBannerText(pinText);
+  }, []);
 
   return (
     <Banner>
       <div className="flex w-full justify-between border-b border-gray-200 bg-gray-50 p-2 dark:border-gray-600 dark:bg-gray-700">
         <div className="mx-auto flex items-center">
           <p className="mt-1 flex items-center text-base text-gray-800 dark:text-gray-400 leading-normal">
-            {pinText}{" "}
+            {bannerText}
           </p>
         </div>
         <Banner.CollapseButton
