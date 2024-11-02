42b0146071a75774e06461b237ad51490a6fbaac
hongji <shihongji21@gmail.com>
2024-11-02 10:31:51 -0700

    alert text refine

--- components/ui/AlertText.tsx
+++ components/ui/AlertText.tsx
@@ -15,9 +15,12 @@ const AlertText: React.FC<AlertTextProps> = ({ gameInsight }) => {
       <h3 className="text-4xl font-semibold text-cp-orange text-center mb-4">
       {gameInsight ? gameInsight.action_type : t('pinned')}
       </h3>
-      <div className="flex items-center justify-between text-2xl my-4">
+      <div className="flex items-center justify-between text-xl my-4">
         <span className="text-gray-700 font-medium">{gameInsight ? gameInsight.action : t('pinnedDescription')}</span>
       </div>
+        <p className="text-gray-600 text-base">{gameInsight ? gameInsight.reason : ""}
+          <span>{gameInsight ? `( ${gameInsight.quarter} - ${gameInsight.time_left_in_quarter} )` : " "}</span>
+        </p>
     </div>
   );
 };
