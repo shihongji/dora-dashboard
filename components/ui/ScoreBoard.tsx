bb3d009e5e3d113690c0ecdd1b32184b6c14adf4
Hongji <shihongji21@gmail.com>
2024-11-02 10:32:48 -0700

    alert text refine (#21)

--- components/ui/ScoreBoard.tsx
+++ components/ui/ScoreBoard.tsx
@@ -2,10 +2,10 @@
 const ScoreBoard = ({ team1, team2, score1, score2, quarter, timeRemaining }: 
     { team1: string; team2: string; score1: number; score2: number; quarter: string; timeRemaining: string }) => {
     return (
-      <div className="bg-cp-dark-blue text-white p-4 rounded-lg shadow-md w-full text-center">
+      <div className="bg-cp-dark-blue text-white p-3 rounded-lg shadow-md w-full text-center">
         {/* Teams and Scores */}
-        <div className="text-5xl font-bold my-2 pt-5 ">
-          {team1}<span className="text-7xl text-cp-yellow"> {score1} : {score2} </span> {team2}
+        <div className="text-4xl font-bold my-2 pt-3 ">
+          {team1}<span className="text-5xl text-cp-yellow"> {score1} : {score2} </span> {team2}
         </div>
         {/* Game Status */}
         <div className="text-lg">
